c7998140ba876069fe6b1e72fafe8d15634de10f
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2024-11-10 10:44:55 +0700

    Lab 05

---
+++ week05_lab_HuynhHoangPhuc_21036541/src/main/java/iuh/week05_lab_huynhhoangphuc_21036541/Week05LabHuynhHoangPhuc21036541Application.java
@@ -0,0 +1,13 @@
+package iuh.week05_lab_huynhhoangphuc_21036541;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week05LabHuynhHoangPhuc21036541Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week05LabHuynhHoangPhuc21036541Application.class, args);
+    }
+
+}
---
+++ week05_lab_HuynhHoangPhuc_21036541/src/test/java/iuh/week05_lab_huynhhoangphuc_21036541/Week05LabHuynhHoangPhuc21036541ApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.week05_lab_huynhhoangphuc_21036541;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week05LabHuynhHoangPhuc21036541ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
